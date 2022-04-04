4ad20e3534844d4c88d42cc7aa688faa1606a679
dticed <lucaspierrealencar@gmail.com>
2022-04-04 19:27:31 +0100

    testing coin api with ajax and jquery
    Signed-off-by: dticed <lucaspierrealencar@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-  $('.navbar-nav li').on('click', 'a', function() {
-    $('.navbar-nav a.active').removeClass('active');
-    $(this).addClass('active');
+  $('.navbar-nav li').on('click', 'a', function () {
+    $('.navbar-nav a.active').removeClass('active')
+    $(this).addClass('active')
+  })
+ 
+  $('#btn-btc').on('click', function() {
+    console.log('meuzovo')
+    $.ajax({
+      type: 'GET',
+      url: 'https://rest.coinapi.io/v1/exchanges/BINANCE?apikey=B8D30D82-84CA-488C-9497-50A3583A2523',
+      success: function (data) {
+        $('#binance').text(data[0].exchange_id)
+        $('#binanceText').text(data[0].website)
+        console.log(data)
+      },
+    })
   })
 })
