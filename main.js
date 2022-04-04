0e76bee971fb0585ddc92af196c12c6d588b047b
dticed <lucaspierrealencar@gmail.com>
2022-04-05 00:02:31 +0100

    fix responsive home layout
    Signed-off-by: dticed <lucaspierrealencar@gmail.com>

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
   })
  
   $('#btn-btc').on('click', function() {
-    console.log('meuzovo')
     $.ajax({
       type: 'GET',
       url: 'https://rest.coinapi.io/v1/exchanges/BINANCE?apikey=B8D30D82-84CA-488C-9497-50A3583A2523',
