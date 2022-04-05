6156857fbfa35ba0016c97ea373452815fc4d9af
dticed <lucaspierrealencar@gmail.com>
2022-04-05 02:15:17 +0100

    testing coingecko api + ajax + jquery
    Signed-off-by: dticed <lucaspierrealencar@gmail.com>

--- main.js
+++ main.js
@@ -3,16 +3,37 @@ $(document).ready(function () {
     $('.navbar-nav a.active').removeClass('active')
     $(this).addClass('active')
   })
- 
-  $('#btn-btc').on('click', function() {
-    $.ajax({
-      type: 'GET',
-      url: 'https://rest.coinapi.io/v1/exchanges/BINANCE?apikey=B8D30D82-84CA-488C-9497-50A3583A2523',
-      success: function (data) {
-        $('#binance').text(data[0].exchange_id)
-        $('#binanceText').text(data[0].website)
-        console.log(data)
-      },
-    })
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin%2C%20ethereum%2C%20solana%2C%20cardano&order=market_cap_desc&per_page=100&page=1&sparkline=false',
+    success: function (data) {
+      data.forEach(element => {
+        var tr = $('<tr></tr>')
+        var td1 = $('<td></td>')
+        var img = $('<img>')
+        img.attr('src', element.image)
+        img.attr('alt', 'btcLogo')
+        img.addClass('crypto-images')
+        td1.append(img)
+  
+        var td2 = $('<td></td>')
+        td2.append(element.name)
+  
+        var td3 = $('<td></td>')
+        td3.append(element.current_price)
+  
+        var td4 = $('<td></td>')
+        td4.append(element.market_cap)
+  
+        tr.append(td1)
+        tr.append(td2)
+        tr.append(td3)
+        tr.append(td4)
+
+        $('.cryptos-table').append(tr)
+      });
+      console.log(data)
+    },
   })
 })
